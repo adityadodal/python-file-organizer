73ec013755bbda09f6182c3bd55bf67b51089799
Aditya Dodal <adityadodal@gmail.com>
2026-01-29 02:15:31 +0530

    Process only files, ignore folders

--- organizer.py
+++ organizer.py
@@ -1,4 +1,16 @@
 import os
 import shutil
 
-print("File Organizer Started")
+TARGET_FOLDER = os.path.expanduser("~/Downloads")
+
+print("Organizing files in:", TARGET_FOLDER)
+
+# List all items in folder
+for item in os.listdir(TARGET_FOLDER):
+    item_path = os.path.join(TARGET_FOLDER, item)
+
+    # Only process files (ignore folders)
+    if os.path.isfile(item_path):
+        print("File:", item)
+    else:
+        print("Skipping folder:", item)
